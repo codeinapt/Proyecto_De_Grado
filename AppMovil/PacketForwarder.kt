29fbf3e869710b3beef660ff49c1e67c0ace6b69
Alejandro Artunduaga <alejandroartunduaga4@gmail.com>
2026-04-27 22:00:39 -0500

    Implementación de lógica Multi-hop: FloodRouter, PacketCache y Forwarder

---
+++ AppMovil/PacketForwarder.kt
@@ -0,0 +1,17 @@
+class PacketForwarder(private val nearbyManager: INearbyManager) {
+
+    // Ahora recibimos el senderEndpoint para no devolverle el paquete al vecino que nos lo dio
+    fun forward(packet: Packet, senderEndpoint: String) {
+        if (packet.ttl > 0) {
+            packet.ttl -= 1
+
+            // Reenvía a todos los vecinos, EXCEPTO al que nos lo envió
+            // (La sintaxis exacta dependerá de cómo tu compañero programó el INearbyManager)
+            nearbyManager.sendToAll(packet, senderEndpoint)
+
+            android.util.Log.d("FLOOD", "Reenviando paquete ${packet.packetId}. Nuevo TTL: ${packet.ttl}. Excluyendo a: $senderEndpoint")
+        } else {
+            android.util.Log.d("FLOOD", "TTL agotado para ${packet.packetId}. No se reenvía.")
+        }
+    }
+}
